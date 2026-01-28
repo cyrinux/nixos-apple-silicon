b46f800faec41a6a283c75fe50eeafd2a0a2075a
Cyril Levis <git@levis.name>
2026-01-28 20:11:00 +0100

    chore: update

--- apple-silicon-support/packages/linux-asahi/default.nix
+++ apple-silicon-support/packages/linux-asahi/default.nix
@@ -25,7 +25,7 @@ let
         owner = "AsahiLinux";
         repo = "linux";
         rev = "b0b5bbf9f67e9063af591e7a1187d37d0f2b8e29";
-        hash = "sha256-Fs20sODkF4bsChqn0nHisei1ddXH99DgdHqogZSPvsI=";
+        hash = "sha256-CWKAyyGuu9jjcwN3Gz98ZjcTLRPQwnpKd32IyD4zsBw=";
       };
 
       kernelPatches = [
